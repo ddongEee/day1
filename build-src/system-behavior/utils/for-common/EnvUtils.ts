11264598aabf97219f988851fea754d8a22b758e
SPONGE-JL <dev2sponge@gmail.com>
2023-01-06 23:12:46 +0900

    [WF] edit deprecated output syntax

--- build-src/system-behavior/utils/for-common/EnvUtils.ts
+++ build-src/system-behavior/utils/for-common/EnvUtils.ts
@@ -6,7 +6,7 @@ import { LogLevelNames } from "loglevel";
 dotenv.config();
 
 const fixedTestExecutionTime = new Date()
-  .toLocaleString("ko-KR", { timeZone: process.env.TZ ?? "Asia/Seoul" })
+  .toLocaleString("en-US", { timeZone: process.env.TZ ?? "Asia/Seoul" })
   .replace(/T/, " ")
   .replace(/\..+/, "");
 
